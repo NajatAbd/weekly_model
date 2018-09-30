0a2c39999ff5361c2541ea7bb45e3de51592201c
Najatabd <NajatAbd@users.noreply.github.com>
2018-09-30 15:18:57 +0300

    Update weekly.py

--- weekly.py
+++ weekly.py
@@ -4,6 +4,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_error, r2_score
+import json
 #import seaborn as sns
 
 
@@ -44,7 +45,12 @@ def weekly_model():
     
     print (" model Score:"+ str(r2_score(y_test, predictions)*100))
     print (" Model sqr err "+ str(mean_squared_error(y_test,predictions)))
+    data = {"model_score": r2_score(y_test, predictions)*100,
+            "model_squarerror": mean_squared_error(y_test,predictions)
+            }
     
+    with open('output_data/result.json', 'w') as outfile:
+            json.dump(data, outfile, indent=4)
     return " model Score:"+ str(r2_score(y_test, predictions)*100)+"\n Model sqr err "+ str(mean_squared_error(y_test,predictions))
        
 
