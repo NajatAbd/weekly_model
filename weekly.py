2c62eaadccc2e7824a5ce8928d446907b9c9b64f
Najatabd <NajatAbd@users.noreply.github.com>
2018-10-02 11:45:28 +0300

    Update weekly.py

--- weekly.py
+++ weekly.py
@@ -12,13 +12,13 @@ def weekly_model():
     print("HEY!!")
     data = [{'name': 'vikash', 'age': 27}, {'name': 'Satyam', 'age': 14}]
     df = pd.DataFrame.from_dict(data, orient='columns')
-    df.to_csv('result.csv', sep='\t')
-    '''
+    df.to_csv('output_data/result.csv', sep='\t')
+   
     data = {"model_score": 100,
             "model_squarerror": 10}        
-    with open('models_folder/47/result.json', 'w') as outfile:
+    with open('output_data/result.json', 'w') as outfile:
                 json.dump(data, outfile, indent=4)
-    '''        
+            
     print("WRONG!")
        
 weekly_model()
