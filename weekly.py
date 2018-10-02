2b244d2988e1166a67b800a627754d8cb9eb89eb
Najatabd <NajatAbd@users.noreply.github.com>
2018-10-02 10:34:40 +0300

    Update weekly.py

--- weekly.py
+++ weekly.py
@@ -1,4 +1,6 @@
 import json
+import pandas as pd
+
 #import seaborn as sns
 
 
@@ -8,13 +10,15 @@ import json
 def weekly_model():
     
     print("HEY!!")
-    
+    data = [{'name': 'vikash', 'age': 27}, {'name': 'Satyam', 'age': 14}]
+    df = pd.DataFrame.from_dict(data, orient='columns')
+    df.to_csv('result.csv', sep='\t')
+    '''
     data = {"model_score": 100,
             "model_squarerror": 10}        
-    
     with open('result.json', 'w') as outfile:
                 json.dump(data, outfile, indent=4)
-            
+    '''        
     print("WRONG!")
        
 weekly_model()
